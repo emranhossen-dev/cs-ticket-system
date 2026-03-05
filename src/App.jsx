cb8c8d43ecc0e1e4e447a029aabe74cdcf40dd75
Emran Hossen <emran40989@gmail.com>
2026-03-05 13:04:26 +0600

    ui build successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
 import Banner from "./Banner";
 import Navbar from "./Navbar";
+import CardData from "./components/CardData";
 import Footer from "./components/Footer";
 
-const fetchCard = async () => {
-  const res = await fetch("/Card.json");
-  return res.json();
-};
-
-
 function App() {
-  
-
   return (
     <>
       <Navbar></Navbar>
-      <Banner
-        
-      ></Banner>
+      <Banner></Banner>
 
-      <main>
-      
-        
+      <main className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 mt-10">
+        <div className="md:col-span-2">
+          <CardData></CardData>
+        </div>
+
+        <div></div>
       </main>
+
       <Footer></Footer>
     </>
   );
