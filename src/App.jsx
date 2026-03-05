ac43aaffaffe430d882d14c0d0c6f5361d541b2f
Emran Hossen <emran40989@gmail.com>
2026-03-05 12:38:42 +0600

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import Banner from "./Banner";
 import Navbar from "./Navbar";
 import Footer from "./components/Footer";
 
+const fetchCard = async () => {
+  const res = await fetch("/Card.json");
+  return res.json();
+};
+
+
 function App() {
   
 
