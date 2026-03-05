33917df98ceebf2f39586be09cbfb1401a953eb4
Emran Hossen <emran40989@gmail.com>
2026-03-05 13:25:06 +0600

    all functionality updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,53 @@
+import { Suspense } from "react";
 import Banner from "./Banner";
 import Navbar from "./Navbar";
 import CardData from "./components/CardData";
+import Action from "./components/Action";
+import { useState } from "react";
 import Footer from "./components/Footer";
 
+const fetchCard = async () => {
+  const res = await fetch("/Card.json");
+  return res.json();
+};
+
 function App() {
+  const cardPromise = fetchCard();
+
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [resolvedCards, setResolvedCards] = useState([]);
+
   return (
     <>
       <Navbar></Navbar>
-      <Banner></Banner>
+      <Banner
+        selectedCount={selectedCards.length}
+        resolvedCount={resolvedCards.length}
+      ></Banner>
 
       <main className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 mt-10">
         <div className="md:col-span-2">
-          <CardData></CardData>
+          <Suspense
+            fallback={
+              <span className="loading loading-spinner text-error"></span>
+            }
+          >
+            <CardData
+              cardPromise={cardPromise}
+              selectedCards={selectedCards}
+              setSelectedCards={setSelectedCards}
+            />
+          </Suspense>
         </div>
-
-        <div></div>
+        <div>
+          <Action
+            selectedCards={selectedCards}
+            setSelectedCards={setSelectedCards}
+            resolvedCards={resolvedCards}
+            setResolvedCards={setResolvedCards}
+          ></Action>
+        </div>
       </main>
-
       <Footer></Footer>
     </>
   );
--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -1,30 +1,28 @@
-import { useEffect, useState } from "react";
+import React, { use } from "react";
 import vectorImg from "../assets/vector.png";
 
-const CardData = () => {
-  const [cardData, setCardData] = useState([]);
-
-  useEffect(() => {
-    fetch("/Card.json")
-      .then((res) => res.json())
-      .then((data) => setCardData(data));
-  }, []);
+const CardData = ({ cardPromise, selectedCards, setSelectedCards }) => {  
+  const cardData = use(cardPromise);
+  console.log(cardData);
+  
+  const availableCards = cardData.filter(
+  (card) => !selectedCards.find((selected) => selected.id === card.id)
+);
 
   return (
     <>
       <h2 className="text-2xl w-full pl-1 text-black my-3 font-semibold">
         Customer Tickets
       </h2>
-
       <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-3 mb-10">
-        {cardData.map((card) => (
-          <div
+        {availableCards.map((card) => (  
+          <div 
             key={card.id}
-            className="card w-full shadow-sm px-3 py-5 bg-white cursor-pointer"
+            className="card w-full shadow-sm my-0 px-3 py-5 bg-white cursor-pointer"
+            onClick={() => setSelectedCards((prev) => [...prev, card])}
           >
-            <div className="cardTitle flex justify-between items-center text-[#001931]">
+            <div className="cardTitle flex justify-between items-center bg-white text-[#001931] py-0 my-0">
               <h2 className="text-lg font-semibold my-4">{card.title}</h2>
-
               <div className="statusbtn inline-flex items-center gap-2 bg-[#B9F8CF] rounded-2xl px-5 py-2">
                 <span className="w-3 h-3 rounded-full bg-[#02A53B]"></span>
                 <span className="text-[#0B5E06] font-semibold text-sm">
@@ -32,21 +30,18 @@ const CardData = () => {
                 </span>
               </div>
             </div>
-
-            <div className="text-justify text-[#627382] mb-2">
+            <div className="descript text-justify text-[#627382] mb-2 py-0 my-0">
               {card.description}
             </div>
-
-            <div className="text-sm flex justify-between items-center text-[#627382] gap-3">
-              <div className="flex items-center gap-2">
-                <p>{card.id}</p>
-                <p className="font-medium">{card.priority}</p>
+            <div className="cardFooter text-sm flex justify-between items-center text-[#627382] gap-3">
+              <div className="leftsection flex justify-between items-center gap-2">
+                <p className="id">{card.id}</p>
+                <p className="priority font-medium">{card.priority}</p>
               </div>
-
-              <div className="flex items-center gap-2">
-                <p>{card.customer}</p>
-                <img src={vectorImg} alt="" />
-                <p>{card.createdAt}</p>
+              <div className="rightsec flex justify-between items-center gap-2">
+                <p className="customerName">{card.customer}</p>
+                <img className="" src={vectorImg} alt="" />
+                <p className="date">{card.createdAt}</p>
               </div>
             </div>
           </div>
